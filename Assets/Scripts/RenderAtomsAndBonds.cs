5a1e9e34f95c8ca56a282faae87e9de67dec6691
shushyam <rohan-rout@users.noreply.github.com>
2018-10-16 20:15:22 -0700

    RDX molecule with GPU instancing and color coding

--- Assets/Scripts/RenderAtomsAndBonds.cs
+++ Assets/Scripts/RenderAtomsAndBonds.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class RenderAtomsAndBonds : MonoBehaviour {
-    public BondPrefab bondPrefab;
-    public AtomPrefab atomPrefab;
+    public Transform bondPrefab;
+    public Transform carbonAtom;
+    public Transform hydrogenAtom;
+    public Transform oxygenAtom;
+    public Transform nitrogenAtom;
+
     public string filePath;
     public double[] newCenter = new double[3];
     private Utility utility = new Utility();
@@ -20,15 +25,10 @@ public class RenderAtomsAndBonds : MonoBehaviour {
             //    utility.atoms[atomIndex].rr0[0] + " , " +
             //    utility.atoms[atomIndex].rr0[1] + " , " +
             //    utility.atoms[atomIndex].rr0[2]);
-            AtomPrefab atom = (AtomPrefab)Instantiate(atomPrefab);
-            atom.Parent = this.gameObject.name;
-            atom.Coordinates = new Vector3(
-                (float)utility.atoms[atomIndex].rr0[0],
-                (float)utility.atoms[atomIndex].rr0[1],
-                (float)utility.atoms[atomIndex].rr0[2]);
+
 
             float[] scaleFactorH = new float[3];
-            for(int index = 0; index < 3; index++)
+            for (int index = 0; index < 3; index++)
             {
                 if (utility.scaleFactor[index] >= 1)
                     scaleFactorH[index] = 2;
@@ -36,37 +36,43 @@ public class RenderAtomsAndBonds : MonoBehaviour {
                     scaleFactorH[index] = (float)0.5;
             }
 
+            Transform atom = null;
+            switch (utility.atoms[atomIndex].iatom)
+            {
+                case "H": //white
+                    atom = Instantiate(hydrogenAtom);
+                     break;
+                case "C": //dark cyan
+                    atom = Instantiate(carbonAtom);
+                    break;
+                case "O": //red
+                    atom = Instantiate(oxygenAtom);
+                    break;
+                case "N": //blue
+                    atom = Instantiate(nitrogenAtom);
+                    break;
+                default:
+                    atom = Instantiate(hydrogenAtom);
+                    break;
+            }
+            atom.transform.parent = this.gameObject.transform;
+            atom.transform.position = new Vector3(
+                (float)utility.atoms[atomIndex].rr0[0],
+                (float)utility.atoms[atomIndex].rr0[1],
+                (float)utility.atoms[atomIndex].rr0[2]);
             switch (utility.atoms[atomIndex].iatom)
             {
                 case "H":
-                    atom.Scale = new Vector3(
+                    atom.transform.localScale = new Vector3(
                         scaleFactorH[0] * atom.transform.localScale.x * (float)utility.scaleFactor[0],
                         scaleFactorH[1] * atom.transform.localScale.y * (float)utility.scaleFactor[1],
                         scaleFactorH[2] * atom.transform.localScale.z * (float)utility.scaleFactor[2]);
                     break;
                 default:
-                    atom.Scale = new Vector3(
+                    atom.transform.localScale = new Vector3(
                         atom.transform.localScale.x * (float)utility.scaleFactor[0],
                         atom.transform.localScale.y * (float)utility.scaleFactor[1],
                         atom.transform.localScale.z * (float)utility.scaleFactor[2]);
-                    break;
-            }
-            switch (utility.atoms[atomIndex].iatom)
-            {
-                case "H": //white
-                    atom.AtomColor = new Color(1, 1, 1, 1);
-                    break;
-                case "C": //dark cyan
-                    atom.AtomColor = new Color(0, 1, 1, 1);
-                    break;
-                case "O": //red
-                    atom.AtomColor = new Color(1, 0, 0, 1);
-                    break;
-                case "N": //blue
-                    atom.AtomColor = new Color(0, 0, 1, 1);
-                    break;
-                default:
-                    atom.AtomColor = new Color(0, 1, 1, 1);
                     break;
             }
         }
@@ -81,21 +87,33 @@ public class RenderAtomsAndBonds : MonoBehaviour {
                 continue;
             for(int neighbourIndex = 1; neighbourIndex <= utility.atomBonds[bondIndex,0]; neighbourIndex++)
             {
-                BondPrefab bond = (BondPrefab)Instantiate(bondPrefab);
-                bond.StartPosition = new Vector3(
+                Transform bond = Instantiate(bondPrefab);
+                //bond.gameObject.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0);
+                bond.transform.parent = this.gameObject.transform;
+
+                Vector3 StartPosition = new Vector3(
                     (float)utility.atoms[bondIndex].rr0[0],
                     (float)utility.atoms[bondIndex].rr0[1],
                     (float)utility.atoms[bondIndex].rr0[2]);
-                bond.EndPosition = new Vector3(
+                Vector3 EndPosition = new Vector3(
                     (float)utility.atoms[utility.atomBonds[bondIndex, neighbourIndex]].rr0[0],
                     (float)utility.atoms[utility.atomBonds[bondIndex, neighbourIndex]].rr0[1],
                     (float)utility.atoms[utility.atomBonds[bondIndex, neighbourIndex]].rr0[2]);
-                bond.BondColor = new Color(1, 1, 1, 0);
-                bond.Parent = this.gameObject.name;
-                bond.Scale = new float[2] { (float)utility.scaleFactor[0], (float)utility.scaleFactor[2] };
+
+                Vector3 position = Vector3.Lerp(StartPosition, EndPosition, (float)0.5);
+                bond.transform.position = position;
+                bond.transform.up = EndPosition - StartPosition;
+
+                float[] scale = new float[2] { (float)utility.scaleFactor[0], (float)utility.scaleFactor[2] };
+                Vector3 newScale = bond.transform.localScale;
+                newScale.x = (float)0.1 * scale[0];
+                newScale.z = (float)0.1 * scale[1];
+                newScale.y = Vector3.Distance(StartPosition, EndPosition) / 2;
+                bond.transform.localScale = newScale;
             }
         }
     } 
+
     // Use this for initialization
     void Start ()
     {
@@ -103,12 +121,15 @@ public class RenderAtomsAndBonds : MonoBehaviour {
         utility.NewCenter = newCenter;
         utility.GetStructureData();
         calledUtility = true;
+        
         RenderAtoms();
         RenderBonds();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
