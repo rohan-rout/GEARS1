0446b216c5d3354dcf873dadb4847ff85acb71ad
shushyam <rohan-rout@users.noreply.github.com>
2018-10-28 19:53:32 -0700

    implementing LOD and Octrees

---
+++ Assets/Scripts/Octree.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OctreeIndex
+{
+	BottomLeftFront = 0,//000,
+	BottomRightFront = 2,//010,
+	BottomRightBack = 3,//011,
+	BottomLeftBack = 1,//001,
+	TopLeftFront = 4,//100,
+	TopRightFront = 6,//110,
+	TopRightBack = 7,//111,
+	TopLeftBack = 5,//101,
+}
+
+public class Octree<TType> {
+	private OctreeNode<TType> node;
+	private int depth;
+
+	public Octree(Vector3 position, float size, int depth)
+	{
+		this.node = new OctreeNode<TType>(position, size);
+		this.node.Subdivide(depth);
+
+;	}
+
+	public class OctreeNode<TType>
+	{
+		public Vector3 position;
+		public float size; 
+		OctreeNode<TType>[] subNodes;
+		IList<TType> value;
+
+		public OctreeNode(Vector3 pos, float size)
+		{
+			this.position = pos;
+			this.size = size;
+		}
+
+		public IEnumerable Nodes
+		{
+			get { return subNodes; }
+		}
+
+		public void Subdivide(int d)
+		{
+			subNodes = new OctreeNode<TType>[8];
+			for (int i = 0; i < subNodes.Length; i++)
+			{
+				Vector3 newPos = this.position;
+
+				newPos.y += ( ( i & 4 ) == 4 ) ? size * 0.25f : -1.0f * size*0.25f;
+				newPos.x += ( ( i & 2 ) == 2 ) ? size * 0.25f : -1.0f * size * 0.25f;
+				newPos.z += ( ( i & 1 ) == 1 ) ? size * 0.25f : -1.0f * size * 0.25f;
+
+				subNodes[i] = new OctreeNode<TType>(newPos, size * 0.5f);
+				if (d > 0)
+				{
+					subNodes[i].Subdivide(d - 1);
+				}
+			}
+		}
+
+		public bool IsLeaf()
+		{
+			return subNodes == null;
+		}
+	}
+
+	private int GetIndexOfPosition(Vector3 lookupPosition, Vector3 nodePosition)
+	{ 
+		int index = 0;
+		index |= lookupPosition.y > nodePosition.y ? 4 : 0;
+		index |= lookupPosition.x > nodePosition.x ? 2 : 0;
+		index |= lookupPosition.z > nodePosition.z ? 1 : 0;
+
+		return index;
+
+	}
+
+	public OctreeNode<TType> GetRoot()
+	{
+		return node;
+	}
+
+}
---
+++ Assets/Scripts/OctreeComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctreeComponent : MonoBehaviour {
+
+	public float size = 5;
+	public int depth = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDrawGizmos()
+	{
+		var Octree = new Octree<int>(this.transform.position, size, depth);
+		DrawNode(Octree.GetRoot());
+
+	}
+
+	private void DrawNode(Octree<int>.OctreeNode<int> node)
+	{
+		if (node.IsLeaf())
+		{
+			Gizmos.color = Color.green;
+		}
+		else
+		{
+			Gizmos.color = Color.blue;
+			foreach (var subnode in node.Nodes)
+			{
+
+				DrawNode((Octree<int>.OctreeNode<int>)subnode);
+			}
+		}
+
+		Gizmos.DrawWireCube(node.position, Vector3.one * node.size);
+	}
+
+}
